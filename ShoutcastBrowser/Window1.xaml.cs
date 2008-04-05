e0973c38c9816b0cd9937c9d6f6b34276f119f81
naeem khedarun <naeem.khedarun@gmail.com>
2008-04-05 23:57:48 +0000

    Initial UI showing stations.

--- ShoutcastBrowser/Window1.xaml.cs
+++ ShoutcastBrowser/Window1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ShoutcastIntegration;
 
 namespace ShoutcastBrowser
 {
@@ -22,6 +24,67 @@ namespace ShoutcastBrowser
         public Window1()
         {
             InitializeComponent();
+            IStationFeedService stationFeedService = CastleWindsorFrameworkHelper.New<IStationFeedService>();
+            stationsListView.ItemsSource = stationFeedService.GetStationList();
+        }
+
+        GridViewColumnHeader _lastHeaderClicked = null;
+        ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
+        void GridViewColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+
+            if (headerClicked != null)
+            {
+                if (_lastHeaderClicked != null)
+                {
+                    _lastHeaderClicked.Column.HeaderTemplate = null;
+                }
+
+                if (headerClicked != _lastHeaderClicked)
+                {
+                    direction = ListSortDirection.Ascending;
+                }
+                else
+                {
+                    if (_lastDirection == ListSortDirection.Ascending)
+                    {
+                        direction = ListSortDirection.Descending;
+                    }
+                    else
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                }
+
+                string header = headerClicked.Column.Header as string;
+                Sort(header, direction);
+
+//                if (direction == ListSortDirection.Ascending)
+//                {
+////                    headerClicked.Column.HeaderTemplate =
+////                      Resources["HeaderTemplateArrowUp"] as DataTemplate;
+//                }
+//                else
+//                {
+//                    headerClicked.Column.HeaderTemplate =
+//                      Resources["HeaderTemplateArrowDown"] as DataTemplate;
+//                }
+
+                _lastHeaderClicked = headerClicked;
+                _lastDirection = direction;
+            }
+        }
+
+
+        private void Sort(string sortBy, ListSortDirection direction)
+        {
+            stationsListView.Items.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortBy, direction);
+            stationsListView.Items.SortDescriptions.Add(sd);
+            stationsListView.Items.Refresh();
         }
     }
 }
